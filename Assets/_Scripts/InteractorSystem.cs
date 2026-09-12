f523ca7034ec86c74e533b89c6b847be1de634b3
Alexussybaka <alex10.svoboda@gmail.com>
2026-09-12 13:34:58 +0200

    KonviceInteractor1

--- Assets/_Scripts/InteractorSystem.cs
+++ Assets/_Scripts/InteractorSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
@@ -7,40 +8,76 @@ public class InteractorSystem : MonoBehaviour
     [SerializeField] PlayerSettings settings;
     [SerializeField] LayerMask interactionLayer;
 
+
+
+    //PlayerControls controls;
+    //InputAction interactAction;
+
+    //private void Awake()
+    //{
+    //    controls = new PlayerControls();
+    //    interactAction = controls.Player.Interact;
+    //}
+
+    //private void OnEnable()
+    //{
+    //    controls.Player.Enable();
+
+    //    interactAction.started += OnInteractionStarted;
+    //}
+
+    //private void OnDisable()
+    //{
+    //    interactAction.started -= OnInteractionStarted;
+
+    //    controls.Player.Disable();
+    //}
+
+    //private void OnInteractionStarted(InputAction.CallbackContext context)
+    //{
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, settings.interactionRange, interactionLayer.value))
+    //    {
+    //        if (hit.transform.TryGetComponent<IInteractable>(out IInteractable interactable))
+    //        {
+    //            interactable.OnInteract();
+    //        }
+    //    }
+    //}
+
     public GameObject currentlyHolding;
+    public Transform handPosititon;
 
-    PlayerControls controls;
-    InputAction interactAction;
-
-    private void Awake()
+    public void TakeItem(GameObject item)
     {
-        controls = new PlayerControls();
-        interactAction = controls.Player.Interact;
-    }
-
-    private void OnEnable()
-    {
-        controls.Player.Enable();
-
-        interactAction.started += OnInteractionStarted;
-    }
-
-    private void OnDisable()
-    {
-        interactAction.started -= OnInteractionStarted;
-
-        controls.Player.Disable();
-    }
-
-    private void OnInteractionStarted(InputAction.CallbackContext context)
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, settings.interactionRange, interactionLayer.value))
+        if (currentlyHolding == null)
         {
-            if (hit.transform.TryGetComponent<IInteractable>(out IInteractable interactable))
+            item.transform.SetParent(handPosititon);
+            item.transform.localPosition = Vector3.zero;
+            item.transform.localScale = Vector3.one;
+            currentlyHolding = item;
+        }
+
+        ProximityPromptScript[] prompts = FindObjectsByType<ProximityPromptScript>(FindObjectsSortMode.None);
+
+        foreach (ProximityPromptScript prompt in prompts)
+        {
+            if (prompt.transform.gameObject.layer == interactionLayer.value)
             {
-                interactable.OnInteract();
+                prompt.GetComponent<ProximityPromptScript>().IsVisible = false;
             }
         }
+    }
+
+    public void DropItem()
+    {
+        currentlyHolding.transform.SetParent(null);
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, settings.interactionRange))
+        {
+            currentlyHolding.transform.position = hit.point;
+        }
+
+        currentlyHolding = null;
     }
 }
---
+++ Assets/_Scripts/KonviceInteractor.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class KonviceInteractor : MonoBehaviour
+{
+    
+}
