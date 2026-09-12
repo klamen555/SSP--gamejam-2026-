ed1a6a074414889da8ff1bc87a3a3905bd8268aa
Alexussybaka <alex10.svoboda@gmail.com>
2026-09-12 15:30:55 +0200

    Konvice4

--- Assets/_Scripts/KonviceInteractor.cs
+++ Assets/_Scripts/KonviceInteractor.cs
@@ -9,16 +9,20 @@ public class KonviceInteractor : MonoBehaviour
 
     public void Water(FlowerStats flowerStats)
     {
+        animator.Rebind();
+
+        if (currentCapacity == 0f) return;
+
         if ((currentCapacity - capacityPerPour) < 0)
         {
             flowerStats.waterLevel += currentCapacity;
             currentCapacity = 0;
-            animator.Play("WaterPlants");
+            animator.Play("WateringFlower");
         }
         else
         {
             currentCapacity -= 3f;
-            animator.Play("WaterPlants");
+            animator.Play("WateringFlower");
             flowerStats.waterLevel += 3f;
         }
     }
