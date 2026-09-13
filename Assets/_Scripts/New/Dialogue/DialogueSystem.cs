ad990539541adfc193b1971c75c6af2f3b3ae15d
Alexussybaka <alex10.svoboda@gmail.com>
2026-09-13 13:06:35 +0200

    Psani pribehu

--- Assets/_Scripts/New/Dialogue/DialogueSystem.cs
+++ Assets/_Scripts/New/Dialogue/DialogueSystem.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 public class DialogueSystem : MonoBehaviour
 {
+    [SerializeField] GameObject textWindow;
+    [SerializeField] GameObject choiseWindow;
     [SerializeField] TextMeshProUGUI dialogueObject;
+    [SerializeField] TextMeshProUGUI button_1;
+    [SerializeField] TextMeshProUGUI button_2;
     [SerializeField] AudioSource audioSource;
 
     private IEnumerator DialogueWriter(DialogueSequenceList dialogueSequenceList)
@@ -18,12 +22,31 @@ public class DialogueSystem : MonoBehaviour
                 audioSource.clip = sequence.AudioClip;
                 audioSource.Play();
             }
-            if (sequence.Author == "null") dialogueObject.text = $"{sequence.Text}";
-            else dialogueObject.text = $"[{sequence.Author}]: {sequence.Text}";
 
-            yield return new WaitForSeconds(sequence.Duration);
+            if (sequence.option1 != "")
+            {
+                choiseWindow.SetActive(true);
+                textWindow.SetActive(false);
+
+                button_1.text = sequence.option1;
+                button_2.text = sequence.option2;
+
+                yield return new WaitForSeconds(sequence.Duration);
+            }
+            else
+            {
+                choiseWindow.SetActive(false);
+                textWindow.SetActive(true);
+                
+                if (sequence.Author == "null") dialogueObject.text = $"{sequence.Text}";
+                else dialogueObject.text = $"[{sequence.Author}]: {sequence.Text}";
+
+                yield return new WaitForSeconds(sequence.Duration);
+            } 
         }
         dialogueObject.text = "";
+        textWindow.SetActive(false);
+        choiseWindow.SetActive(false);
     }
 
     public void TriggerDialogue(DialogueSequenceList dialogueSequence)
